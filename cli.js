c82cddf04a231f45a849bb16a444c0ffba8796d2
hardwaresoftwork <760988+hardwaresoftwork@users.noreply.github.com>
2023-06-10 14:37:22 +0200

    allow global install as well as cli usage

--- cli.js
+++ cli.js
@@ -1,7 +1,8 @@
 #!/usr/bin/env node
+const sdk = require('./sdk');
 
 const args = process.argv.slice(2);
-
+let log = console.log;
 // supported arguments
 const supported = ['test', 'release'];
 
@@ -10,10 +11,41 @@ if (!args.length) {
     process.exit();
 } else {
     const method = args[0];
+    let isRelease;
+
     if (supported.indexOf(method) === -1) {
         console.log("Warning: Method no supported");
         process.exit();
     }
+
+    isRelease = method === 'release';
+
+    console.table(args);
+    // process.exit();
+
+    let dry = false;
+    const versionKeys = [];
+    args.forEach(function (val, index, array) {
+        if ((val || '') === '--dry') {
+            dry = true;
+            log('DRY RUN ENABLED')
+        } else {
+            log('no')
+            log(val);
+        }
+        let value = val || '';
+        if (isRelease && value.length && val !== 'release' && val !== '--dry') versionKeys.push(value);
+    });
+
     console.log('Yo: ' + method);
     // TODO implement method handling
+
+    console.log('Versions to build:');
+    console.table(versionKeys);
+    // process.exit();
+
+    if (isRelease) {
+        const versionsToBuild = versionKeys.length ? versionKeys : ['version'];
+        sdk.release.doRelease({versionKeys: versionsToBuild, dry})
+    }
 }
