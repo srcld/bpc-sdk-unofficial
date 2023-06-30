57494a09f54ca0b647d7d2957a4830fe575c827c
hardwaresoftwork <760988+hardwaresoftwork@users.noreply.github.com>
2023-06-30 19:02:47 +0200

    tests / examples modified

--- test.js
+++ test.js
@@ -1,14 +1,20 @@
 const sdk = require('./sdk');
+const utils = require('./utils');
 
 // // super simple example
-sdk.buildPackage('example', 'Example Project', {}).then(() => {
-    // war only..
-    // sdk.getBPCBuilder('example').clean('build-tmp')
-    sdk.getBPCBuilder('example').clean('./build/example')
-})
+// sdk.buildPackage('example', 'Example Project', {}).then(() => {
+//     // war only..
+//     // sdk.getBPCBuilder('example').clean('build-tmp')
+//     sdk.getBPCBuilder('example').clean('./build/example')
+// })
+
+// buildInfo example
+// let buildInfo = utils.getBuildInfo();
+// console.log(buildInfo);
 
 // converted grouped to sorted
 // sdk.migrateSettingFile('default_module_settings.json')
 
 // handle white space
 //sdk.release.updateVersionInGradleProperties('tests/gradle.properties');
+
