bdd58be4f6adb37719caa0a78ce7fbcde2464c9a
hardwaresoftwork <760988+hardwaresoftwork@users.noreply.github.com>
2023-10-01 19:15:29 +0200

    naming improved cleanup

--- cli.js
+++ cli.js
@@ -5,8 +5,7 @@ const {handle} = require("./cli/methods/map");
 const args = process.argv.slice(2);
 let log = console.log;
 
-// supported arguments
-const supported = ['test', 'release', 'build', 'xml'];
+const supportedArgumentNames = ['test', 'release', 'build', 'xml'];
 
 log('BPC SDK - unofficial');
 
@@ -15,20 +14,17 @@ if (!args.length) {
     process.exit();
 } else {
     const method = args[0];
-    let isRelease;
 
-    if (supported.indexOf(method) === -1) {
+    if (supportedArgumentNames.indexOf(method) === -1) {
         log("Warning: Method no supported");
         process.exit();
     }
 
-    // isRelease = method === 'release';
     if (handle[method]) {
         log(method + ' DETECTED');
-        let methodDetected = detectRepo();
-        if (methodDetected === method) {
+        if (detectRepo() === method) {
             log('nice.')
-            handle[method](args, true);
+            handle[method](args);
         } else {
             log('Method wont run. Sorry. Consult the docs.')
         }
