d3dcf6c7227be5eb7d2bb4e95f088372cb0f403b
hwsw <erik@muzkat.com>
2023-10-03 11:04:03 +0200

    support alias log levels

--- utils/log/sourlog.js
+++ utils/log/sourlog.js
@@ -17,9 +17,19 @@ const log = console.log;
 const partsDelimiter = ' - ';
 const defaultLevel = 'info';
 
+const obtainLevel = function (levelOrAlias) {
+    const levels = Object.keys(logLevel);
+    if (levels.indexOf(levelOrAlias) !== -1) return levelOrAlias;
+    const aliasLevels = levels.filter((level) => {
+        let {aliases = []} = logLevel[level];
+        return aliases.indexOf(levelOrAlias) !== -1
+    })
+    return aliasLevels.length ? aliasLevels[0] : defaultLevel;
+}
+
 const doLog = function (msg = '', level = defaultLevel, data = undefined) {
     let isoDate = new Date().toISOString();
-    level = (level || defaultLevel).toUpperCase();
+    level = obtainLevel(level).toUpperCase();
     log([isoDate, level, msg].join(partsDelimiter));
 }
 
